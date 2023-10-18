6ff5dfe4aad06e27d4e3b27eb1433f59e46a0532
Jake Zur <jakezur@umich.edu>
2023-10-18 03:01:53 -0400

    HUGE, got our actions in with error checking. now need to check for stop losses and look for exit opportunities

--- algo.py
+++ algo.py
@@ -4,6 +4,8 @@ import numpy as np
 import statsmodels.api as sm
 
 from pathlib import Path
+from datetime import datetime
+from pydantic import BaseModel
 from sklearn.decomposition import PCA
 from sklearn.preprocessing import StandardScaler
 from sklearn.cluster import KMeans
@@ -11,8 +13,15 @@ from statsmodels.tsa.stattools import coint
 from statsmodels.tsa.stattools import adfuller
 
 
+class Trade(BaseModel):
+    stock_a: str
+    stock_b: str
+    price_a: float
+    price_b: float
+
+
 class Algo:
-    def __init__(self, data_path: Path, cash=25000, n_components=30, n_clusters=8, use_log_returns=False):
+    def __init__(self, data_path: Path, cash=25000, n_components=30, n_clusters=5, use_log_returns=False):
         warnings.simplefilter(action='ignore', category=Warning)
 
         self.original_data = pd.read_csv(data_path)
@@ -20,11 +29,12 @@ class Algo:
         self.original_data.set_index(['Ticker', 'Date'], inplace=True)
 
         self.use_log_returns = use_log_returns
-        self.original_data['Ret'] = self.original_data['Adj Close'].groupby('Ticker').pct_change()
+        self.original_data['Ret'] = self.original_data['Open'].groupby('Ticker').pct_change()
         self.original_data.dropna(inplace=True)
         self.returns = self.original_data['Ret'].unstack(level='Ticker')
         if use_log_returns:
-            self.original_data['Log Ret'] = np.log(self.original_data['Adj Close'] / self.original_data['Adj Close'].groupby('Ticker').shift(1))
+            self.original_data['Log Ret'] = np.log(
+                self.original_data['Open'] / self.original_data['Open'].groupby('Ticker').shift(1))
             self.original_data.dropna(inplace=True)
             self.log_returns = self.original_data['Log Ret'].unstack(level='Ticker')
 
@@ -32,8 +42,10 @@ class Algo:
         self.n_clusters = n_clusters
 
         self.actions = pd.DataFrame(columns=self.returns.columns)
-        self.positions = [0] * len(self.returns.columns)
+        self.positions = dict(zip(self.returns.columns, [0] * len(self.returns.columns)))
+        self.open_trades: list[Trade] = []
         self.cash = cash
+        self.assets = 0
         self.debt = 0
 
     def run_pairs(self):
@@ -85,8 +97,35 @@ class Algo:
                 curr_pairs = sorted(curr_pairs)
                 filtered_pairs, pairs = self.initialize_month(curr_data=curr_data, pairs=pairs, curr_pairs=curr_pairs)
 
-                print('hello')
+                days_actions = pd.Series()
+                days_actions_data = dict(zip(self.returns.columns, [0] * len(self.returns.columns)))
+                for stationarity, (stock_a, stock_b) in filtered_pairs:
+                    pair_name = '(' + stock_a + ', ' + stock_b + ')'
+                    # this is trading logic
+                    if pairs[pair_name + '_zscore'].iloc[-1] < \
+                            pairs[pair_name + '_lower_threshold'].iloc[-1]:
+                        request_successful, days_actions_data = self.request_a_long_b_short(
+                            date=pairs.index[-1],
+                            days_actions=days_actions_data,
+                            stock_a=stock_a,
+                            price_a=self.original_data.loc[stock_a, 'Open'].iloc[-1],
+                            stock_b=stock_b,
+                            price_b=self.original_data.loc[stock_b, 'Open'].iloc[-1])
 
+                    elif pairs[pair_name + '_zscore'].iloc[-1] > \
+                            pairs[pair_name + '_upper_threshold'].iloc[-1]:
+                        request_successful, days_actions_data = self.request_a_short_b_long(
+                            date=pairs.index[-1],
+                            days_actions=days_actions_data,
+                            stock_a=stock_a,
+                            price_a=self.original_data.loc[stock_a, 'Open'].iloc[-1],
+                            stock_b=stock_b,
+                            price_b=self.original_data.loc[stock_b, 'Open'].iloc[-1])
+
+                days_actions = pd.Series(data=days_actions_data, name=pairs.index[-1])
+                self.actions = pd.concat([self.actions, days_actions])
+
+                print('hello')
         # pairs_spreads = pd.DataFrame()
         # for index, row in enumerate(data):
         # EVERY MONTH WE GET NEW PAIRS TO TRADE
@@ -100,16 +139,46 @@ class Algo:
         # for pair in pairs.columns:
         # trade
 
+    def request_a_short_b_long(self, date: datetime, days_actions: dict, stock_a: str, stock_b: str, price_a: float, price_b: float) -> (bool, dict):
+        if self.debt + price_a < self.cash + self.assets and self.cash > price_b:
+            self.positions[stock_a] -= 1
+            self.positions[stock_b] += 1
+            self.debt += price_a
+            self.cash -= price_b
+            self.assets += price_b
+            days_actions[stock_a] -= 1
+            days_actions[stock_b] += 1
+            self.open_trades.append(Trade(stock_a=stock_a, stock_b=stock_b, price_a=-price_a, price_b=price_b))
+
+            return True, days_actions
+        else:
+            return False
+
+    def request_a_long_b_short(self, date: datetime, days_actions: dict, stock_a: str, stock_b: str, price_a: float, price_b: float) -> (bool, dict):
+        if self.debt + price_b < self.cash + self.assets and self.cash > price_a:
+            self.positions[stock_a] += 1
+            self.positions[stock_b] -= 1
+            self.debt += price_b
+            self.cash -= price_a
+            self.assets += price_a
+            days_actions[stock_a] += 1
+            days_actions[stock_b] -= 1
+            self.open_trades.append(Trade(stock_a=stock_a, stock_b=stock_b, price_a=price_a, price_b=-price_b))
+
+            return True, days_actions
+        else:
+            return False
+
     @staticmethod
     def initialize_month(curr_data: pd.DataFrame, pairs: pd.DataFrame, curr_pairs: list[tuple]) \
             -> (list[tuple], pd.DataFrame):
         filtered_pairs = []
         for stock_a, stock_b in curr_pairs:
-            # converts into resid = stock_b - m(stock_a)
-            stock_a_preprocessed = sm.add_constant(curr_data[stock_a])
+            # converts into resid = stock_a - m(stock_b)
+            stock_b_preprocessed = sm.add_constant(curr_data[stock_b])
 
             # Run OLS regression
-            model = sm.OLS(curr_data[stock_b], stock_a_preprocessed).fit()
+            model = sm.OLS(curr_data[stock_a], stock_b_preprocessed).fit()
             pair_name = '(' + stock_a + ', ' + stock_b + ')'
 
             # we are checking for stationarity of the residuals now to make sure the spreads themselves are mean
@@ -121,6 +190,11 @@ class Algo:
                 filtered_pairs.append((residual_stationarity, (stock_a, stock_b)))
                 pairs[pair_name] = model.resid
                 pairs[pair_name + '_mean'] = pairs[pair_name].mean()
+                pairs[pair_name + '_zscore'] = (pairs[pair_name] - pairs[pair_name + '_mean']) / pairs[pair_name].std()
+                pairs[pair_name + '_upper_threshold'] = pairs[pair_name + '_zscore'].mean() + (
+                        2 * pairs[pair_name + '_zscore'].std())
+                pairs[pair_name + '_lower_threshold'] = pairs[pair_name + '_zscore'].mean() - (
+                        2 * pairs[pair_name + '_zscore'].std())
 
         return sorted(filtered_pairs), pairs
 
