fd0c3ace0d19c4072d4e6c16070e8c89ce27e237
Jake Zur <jakezur@umich.edu>
2023-10-22 16:58:05 -0400

    did a shit ton

--- algo.py
+++ algo.py
@@ -2,10 +2,12 @@ import warnings
 import pandas as pd
 import numpy as np
 import statsmodels.api as sm
+import matplotlib.pyplot as plt
 
 from pathlib import Path
 from datetime import datetime
 from pydantic import BaseModel
+from tqdm.auto import tqdm
 from sklearn.decomposition import PCA
 from sklearn.preprocessing import StandardScaler
 from sklearn.cluster import KMeans
@@ -17,11 +19,13 @@ class Trade(BaseModel):
     stock_a: str
     stock_b: str
     price_a: float
+    num_a: int
     price_b: float
+    num_b: int
 
 
 class Algo:
-    def __init__(self, data_path: Path, cash=25000, n_components=30, n_clusters=5, use_log_returns=False):
+    def __init__(self, data_path: Path, cash=25000, n_components=12, n_clusters=6, use_log_returns=False, even_weight_multiplier=10):
         warnings.simplefilter(action='ignore', category=Warning)
 
         self.original_data = pd.read_csv(data_path)
@@ -40,8 +44,9 @@ class Algo:
 
         self.n_components = n_components
         self.n_clusters = n_clusters
+        self.even_weight_multiplier = even_weight_multiplier
 
-        self.actions = pd.DataFrame(columns=self.returns.columns)
+        self.actions = pd.DataFrame()
         self.positions = dict(zip(self.returns.columns, [0] * len(self.returns.columns)))
         self.open_trades: list[Trade] = []
         self.cash = cash
@@ -50,19 +55,22 @@ class Algo:
 
     def run_pairs(self):
         pairs = pd.DataFrame()
+        filtered_pairs = {}
         curr_pairs = []
-        look_back_window = 30
-        for index, (date, row) in enumerate(self.returns.iloc[look_back_window - 1:].iterrows()):
-            if index % look_back_window == 0:
+        look_back_window = 60
+        day = look_back_window
+        total = len(self.returns.iloc[look_back_window - 1:])
+        for index, row in tqdm(enumerate(self.returns.iloc[look_back_window - 1:].iterrows()), total=total):
+            if self.use_log_returns:
+                curr_data = self.log_returns.iloc[day - look_back_window: day]
+            else:
+                curr_data = self.returns.iloc[day - look_back_window: day]
+            stocks = list(curr_data.columns)
+            # get pairs it's if the first of the month
+            if day % look_back_window == 0:
+                # TODO: need to finish up open trades here
                 pairs = pd.DataFrame()
                 curr_pairs = []
-
-                # get last 60 days of data for PCA
-                if self.use_log_returns:
-                    curr_data = self.log_returns.iloc[index: index + look_back_window]
-                else:
-                    curr_data = self.returns.iloc[index: index + look_back_window]
-                stocks = list(curr_data.columns)
 
                 # scale data and run PCA
                 scaler = StandardScaler()
@@ -83,97 +91,124 @@ class Algo:
                     clusters[cluster].append(stock)
 
                 for cluster in clusters.values():
-                    for i in range(len(cluster)):
-                        for j in range(i + 1, len(cluster)):
-                            # WE DON'T NEED THIS. COINTEGRATION ONLY MATTERS ON PRICES, WE DON'T NEED IT
-                            # ON LOGGED RETURNS OR RETURNS
-
-                            # _, coint_p_val, _ = coint(curr_data[cluster[i]], curr_data[cluster[j]])
-                            # a_stationary = adfuller(curr_data[cluster[i]])[1]
-                            # b_stationary = adfuller(curr_data[cluster[j]])[1]
-                            # if coint_p_val < 0.05 and a_stationary < 0.05 and b_stationary < 0.05:
-                            curr_pairs.append((cluster[i], cluster[j]))
+                    if len(cluster) > 3:
+                        for i in range(len(cluster)):
+                            for j in range(i + 1, len(cluster)):
+                                curr_pairs.append((cluster[i], cluster[j]))
 
                 curr_pairs = sorted(curr_pairs)
-                filtered_pairs, pairs = self.initialize_month(curr_data=curr_data, pairs=pairs, curr_pairs=curr_pairs)
 
-                days_actions = pd.Series()
-                days_actions_data = dict(zip(self.returns.columns, [0] * len(self.returns.columns)))
-                for stationarity, (stock_a, stock_b) in filtered_pairs:
-                    pair_name = '(' + stock_a + ', ' + stock_b + ')'
-                    # this is trading logic
-                    if pairs[pair_name + '_zscore'].iloc[-1] < \
-                            pairs[pair_name + '_lower_threshold'].iloc[-1]:
-                        request_successful, days_actions_data = self.request_a_long_b_short(
-                            date=pairs.index[-1],
-                            days_actions=days_actions_data,
-                            stock_a=stock_a,
-                            price_a=self.original_data.loc[stock_a, 'Open'].iloc[-1],
-                            stock_b=stock_b,
-                            price_b=self.original_data.loc[stock_b, 'Open'].iloc[-1])
+            initial = False
+            if not index:
+                initial = True
 
-                    elif pairs[pair_name + '_zscore'].iloc[-1] > \
-                            pairs[pair_name + '_upper_threshold'].iloc[-1]:
-                        request_successful, days_actions_data = self.request_a_short_b_long(
-                            date=pairs.index[-1],
-                            days_actions=days_actions_data,
-                            stock_a=stock_a,
-                            price_a=self.original_data.loc[stock_a, 'Open'].iloc[-1],
-                            stock_b=stock_b,
-                            price_b=self.original_data.loc[stock_b, 'Open'].iloc[-1])
+            filtered_pairs, pairs = self.update_data(curr_data=curr_data, curr_pairs=curr_pairs, initial_update=initial)
 
-                days_actions = pd.Series(data=days_actions_data, name=pairs.index[-1])
-                self.actions = pd.concat([self.actions, days_actions])
+            # TODO: add stop loss logic to exit positions
+            self.exit_positions(pairs)
+            self.trade_pairs(filtered_pairs, pairs)
+            day += 1
 
-                print('hello')
-        # pairs_spreads = pd.DataFrame()
-        # for index, row in enumerate(data):
-        # EVERY MONTH WE GET NEW PAIRS TO TRADE
-        # if index % 30 == 0:
-        # run pca on [i - 60: i]
-        # get clusters for stocks
-        # calc initial averages and put into pairs spreads
-        # trade on this day
-        # else
-        # get spread of today and updates pairs_spreads
-        # for pair in pairs.columns:
-        # trade
+        print('hello')
 
-    def request_a_short_b_long(self, date: datetime, days_actions: dict, stock_a: str, stock_b: str, price_a: float, price_b: float) -> (bool, dict):
-        if self.debt + price_a < self.cash + self.assets and self.cash > price_b:
-            self.positions[stock_a] -= 1
-            self.positions[stock_b] += 1
-            self.debt += price_a
-            self.cash -= price_b
-            self.assets += price_b
-            days_actions[stock_a] -= 1
-            days_actions[stock_b] += 1
-            self.open_trades.append(Trade(stock_a=stock_a, stock_b=stock_b, price_a=-price_a, price_b=price_b))
+    def trade_pairs(self, filtered_pairs: dict, pairs: pd.DataFrame):
+        days_actions_data = dict(zip(self.returns.columns, [0] * len(self.returns.columns)))
+        for stationarity, (stock_a, stock_b) in filtered_pairs:
+            pair_name = '(' + stock_a + ', ' + stock_b + ')'
+            # this is trading logic
+            if pairs[pair_name + '_zscore'].iloc[-1] < \
+                    pairs[pair_name + '_lower_threshold'].iloc[-1]:
+                request_successful, days_actions_data = self.request_a_long_b_short(
+                    days_actions=days_actions_data,
+                    stock_a=stock_a,
+                    price_a=self.original_data.loc[stock_a, 'Open'].loc[str(pairs.index.date[-1])],
+                    stock_b=stock_b,
+                    price_b=self.original_data.loc[stock_b, 'Open'].loc[str(pairs.index.date[-1])])
+
+            elif pairs[pair_name + '_zscore'].iloc[-1] > \
+                    pairs[pair_name + '_upper_threshold'].iloc[-1]:
+                request_successful, days_actions_data = self.request_a_short_b_long(
+                    days_actions=days_actions_data,
+                    stock_a=stock_a,
+                    price_a=self.original_data.loc[stock_a, 'Open'].loc[str(pairs.index.date[-1])],
+                    stock_b=stock_b,
+                    price_b=self.original_data.loc[stock_b, 'Open'].loc[str(pairs.index.date[-1])])
+
+        days_actions = pd.Series(data=days_actions_data, name=pairs.index[-1])
+        self.actions = pd.concat([self.actions, days_actions], axis=1)
+
+    def request_a_short_b_long(self, days_actions: dict, stock_a: str, stock_b: str, price_a: float,
+                               price_b: float) -> (bool, dict):
+        if self.debt + (price_a * self.even_weight_multiplier) < self.cash + self.assets and self.cash > price_b and \
+                (self.actions.empty or (self.actions.iloc[:, -10:].loc[stock_a].abs().sum() < 5 * self.even_weight_multiplier
+                                        and self.actions.iloc[:, -10:].loc[stock_b].abs().sum() < 5 * self.even_weight_multiplier)):
+            self.positions[stock_a] -= 1 * self.even_weight_multiplier
+            self.positions[stock_b] += 1 * self.even_weight_multiplier
+            self.debt += price_a * self.even_weight_multiplier
+            self.cash -= price_b * self.even_weight_multiplier
+            self.assets += price_b * self.even_weight_multiplier
+            days_actions[stock_a] -= 1 * self.even_weight_multiplier
+            days_actions[stock_b] += 1 * self.even_weight_multiplier
+            self.open_trades.append(Trade(stock_a=stock_a, stock_b=stock_b, price_a=price_a, num_a=-1*self.even_weight_multiplier, price_b=price_b, num_b=self.even_weight_multiplier))
+            return True, days_actions
+        else:
+            return False, days_actions
+
+    def request_a_long_b_short(self, days_actions: dict, stock_a: str, stock_b: str, price_a: float,
+                               price_b: float) -> (bool, dict):
+        if self.debt + (price_b * self.even_weight_multiplier) < self.cash + self.assets and self.cash > price_a and \
+                (self.actions.empty or (self.actions.iloc[:, -10:].loc[stock_a].abs().sum() < 5 * self.even_weight_multiplier
+                                        and self.actions.iloc[:, -10:].loc[stock_b].abs().sum() < 5 * self.even_weight_multiplier)):
+            self.positions[stock_a] += 1 * self.even_weight_multiplier
+            self.positions[stock_b] -= 1 * self.even_weight_multiplier
+            self.debt += price_b * self.even_weight_multiplier
+            self.cash -= price_a * self.even_weight_multiplier
+            self.assets += price_a * self.even_weight_multiplier
+            days_actions[stock_a] += 1 * self.even_weight_multiplier
+            days_actions[stock_b] -= 1 * self.even_weight_multiplier
+            self.open_trades.append(Trade(stock_a=stock_a, stock_b=stock_b, price_a=price_a, num_a=self.even_weight_multiplier, price_b=price_b, num_b=-1*self.even_weight_multiplier))
 
             return True, days_actions
         else:
-            return False
+            return False, days_actions
+    
+    def exit_positions(self, pairs):
+        indices_to_delete = []
+        for index, trade in enumerate(self.open_trades.copy()):
+            curr_a_price = self.original_data.loc[trade.stock_a, 'Open'].loc[str(pairs.index.date[-1])]
+            curr_b_price = self.original_data.loc[trade.stock_b, 'Adj Close'].loc[str(pairs.index.date[-1])]
+            pair_name = '(' + trade.stock_a + ', ' + trade.stock_b + ')'
+            if trade.num_a < 0:
+                if pairs[pair_name + '_zscore'].iloc[-1] <= pairs[pair_name + '_mean'][-1]:
+                    self.positions[trade.stock_a] -= trade.num_a
+                    self.cash += (curr_a_price - trade.price_a) * abs(trade.num_a)
+                    self.debt -= trade.price_a * abs(trade.num_a)
 
-    def request_a_long_b_short(self, date: datetime, days_actions: dict, stock_a: str, stock_b: str, price_a: float, price_b: float) -> (bool, dict):
-        if self.debt + price_b < self.cash + self.assets and self.cash > price_a:
-            self.positions[stock_a] += 1
-            self.positions[stock_b] -= 1
-            self.debt += price_b
-            self.cash -= price_a
-            self.assets += price_a
-            days_actions[stock_a] += 1
-            days_actions[stock_b] -= 1
-            self.open_trades.append(Trade(stock_a=stock_a, stock_b=stock_b, price_a=price_a, price_b=-price_b))
+                    self.positions[trade.stock_b] -= trade.num_b
+                    self.cash += curr_b_price * abs(trade.num_b)
+                    self.assets -= trade.price_b * abs(trade.num_b)
+                    indices_to_delete.append(index)
 
-            return True, days_actions
-        else:
-            return False
+            if trade.num_b < 0:
+                if pairs[pair_name + '_zscore'].iloc[-1] >= pairs[pair_name + '_mean'][-1]:
+                    self.positions[trade.stock_b] -= trade.num_b
+                    self.cash += (curr_b_price - trade.price_b) * abs(trade.num_b)
+                    self.debt -= trade.price_b * abs(trade.num_b)
 
-    @staticmethod
-    def initialize_month(curr_data: pd.DataFrame, pairs: pd.DataFrame, curr_pairs: list[tuple]) \
+                    self.positions[trade.stock_a] -= trade.num_a
+                    self.cash += curr_a_price * abs(trade.num_a)
+                    self.assets -= trade.price_a * abs(trade.num_a)
+                    indices_to_delete.append(index)
+
+        for index in reversed(indices_to_delete):
+            self.open_trades.remove(self.open_trades[index])
+
+    def update_data(self, curr_data: pd.DataFrame, curr_pairs: list[tuple], initial_update=False) \
             -> (list[tuple], pd.DataFrame):
+        pairs = pd.DataFrame()
         filtered_pairs = []
-        for stock_a, stock_b in curr_pairs:
+        curr_pairs_to_delete = []
+        for index, (stock_a, stock_b) in enumerate(curr_pairs.copy()):
             # converts into resid = stock_a - m(stock_b)
             stock_b_preprocessed = sm.add_constant(curr_data[stock_b])
 
@@ -181,12 +216,40 @@ class Algo:
             model = sm.OLS(curr_data[stock_a], stock_b_preprocessed).fit()
             pair_name = '(' + stock_a + ', ' + stock_b + ')'
 
+            # ensure that the original stocks are not stationary, otherwise they can't be cointegrated
+            if initial_update:
+                stationary_a = adfuller(self.original_data.loc[stock_a, 'Open'].loc[curr_data.index])[1]
+                stationary_b = adfuller(self.original_data.loc[stock_b, 'Open'].loc[curr_data.index])[1]
+                if stationary_a < 0.05 and stationary_b < 0.05:
+                    curr_pairs_to_delete.append(index)
+                    continue
+
             # we are checking for stationarity of the residuals now to make sure the spreads themselves are mean
             # reverting. before this would work if on the original series if we were doing prices, because it would
             # just be the difference between them. but now since we are doing a regression, we need to check for
             # stationarity after we find our pairs, then filter them down here with an adfuller test
-            residual_stationarity = adfuller(model.resid)[1]
-            if residual_stationarity < 0.05:
+            hedge_ratio = model.params[1]
+            spread = curr_data[stock_a] - hedge_ratio * curr_data[stock_b]
+            residual_stationarity = adfuller(spread)[1]
+
+            # spread_lag = spread.shift(1).dropna()
+            # spread_lag, spread = spread_lag.align(spread)
+            # spread_lag = sm.add_constant(spread_lag)
+            # model = sm.OLS(spread, spread_lag).fit()
+            lambda_estimate = model.params[1]
+            if initial_update:
+                if residual_stationarity < 0.05:
+                    filtered_pairs.append((residual_stationarity, (stock_a, stock_b)))
+                    pairs[pair_name] = spread
+                    pairs[pair_name + '_mean'] = pairs[pair_name].mean()
+                    pairs[pair_name + '_zscore'] = (pairs[pair_name] - pairs[pair_name + '_mean']) / pairs[pair_name].std()
+                    pairs[pair_name + '_upper_threshold'] = pairs[pair_name + '_zscore'].mean() + (
+                            2 * pairs[pair_name + '_zscore'].std())
+                    pairs[pair_name + '_lower_threshold'] = pairs[pair_name + '_zscore'].mean() - (
+                            2 * pairs[pair_name + '_zscore'].std())
+                else:
+                    curr_pairs_to_delete.append(index)
+            else:
                 filtered_pairs.append((residual_stationarity, (stock_a, stock_b)))
                 pairs[pair_name] = model.resid
                 pairs[pair_name + '_mean'] = pairs[pair_name].mean()
@@ -196,7 +259,46 @@ class Algo:
                 pairs[pair_name + '_lower_threshold'] = pairs[pair_name + '_zscore'].mean() - (
                         2 * pairs[pair_name + '_zscore'].std())
 
+            self.plot_zscores(pairs, pair_name, stock_a, stock_b)
+
+        for index in reversed(curr_pairs_to_delete):
+            curr_pairs.remove(curr_pairs[index])
+
         return sorted(filtered_pairs), pairs
+
+    def plot_zscores(self, pairs_df, pair_name, stock_a, stock_b):
+        # Extracting z-score data
+        zscores = pairs_df[pair_name + '_zscore']
+        upper_threshold = pairs_df[pair_name + '_upper_threshold']
+        lower_threshold = pairs_df[pair_name + '_lower_threshold']
+        mean_zscore = pairs_df[pair_name + '_mean']
+
+        price_a = self.original_data.loc[stock_a, 'Open'].loc[pairs_df.index]
+        price_b = self.original_data.loc[stock_b, 'Open'].loc[pairs_df.index]
+
+        # Plotting
+        fig, ax1 = plt.subplots(figsize=(15, 7))
+
+        # Plotting z-scores on primary y-axis
+        ax1.plot(zscores.index, zscores, label='Z-Score', color='blue')
+        ax1.plot(zscores.index, upper_threshold, label='Upper Threshold', linestyle='--', color='red')
+        ax1.plot(zscores.index, lower_threshold, label='Lower Threshold', linestyle='--', color='green')
+        ax1.plot(zscores.index, mean_zscore, color='black', linestyle='--', label='Mean')
+        ax1.set_xlabel('Date')
+        ax1.set_ylabel('Z-Score', color='blue')
+        ax1.legend(loc='upper left')
+        ax1.tick_params(axis='y', labelcolor='blue')
+        ax1.grid(True)
+
+        ax2 = ax1.twinx()
+        ax2.plot(price_a.index, price_a, label=f'Price {stock_a}', color='magenta')
+        ax2.plot(price_b.index, price_b, label=f'Price {stock_b}', color='cyan')
+        ax2.set_ylabel('Stock Price', color='magenta')
+        ax2.legend(loc='upper right')
+        ax2.tick_params(axis='y', labelcolor='magenta')
+
+        plt.title(f'Z-Scores & Prices for {pair_name}')
+        plt.show()
 
 
 if __name__ == '__main__':
